35789bc96108a8a83ca0fff8db44ef5e4a10ea6b
ZhuravlevaIrina2012 <69622507+ZhuravlevaIrina2012@users.noreply.github.com>
2020-10-18 12:06:02 +0300

    Update fibonacci.js
    Added checking parameter> 2 and clearing when pressing the button again

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,16 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click', printFibonacci);
 const res = document.getElementById('p');
+const input = document.getElementById('num');
 
 const Fib = {
     [Symbol.iterator]() {
-        const num = document.getElementById('num').value;
+        const num = input.value;
+        if (num < 2){
+            input.value = '';
+            alert('Wrong parameter');
+            return null;
+        }
         let count = 0;
 
         let n1 = 1;
@@ -27,6 +33,9 @@ const Fib = {
 };
 
 function printFibonacci() {
+    while (res.firstChild){
+        res.removeChild(res.firstChild);
+    }
     for (const n of Fib) {
         res.append(`${n} `);
     }
